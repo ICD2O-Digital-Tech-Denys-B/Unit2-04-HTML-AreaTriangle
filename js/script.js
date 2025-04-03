83cbb0107042e06e3141a62196d10076304cfe75
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-04-03 14:36:12 +0000

    Finished

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+//Function that gets the base and height value, calculates the area and inserts text into the <div> tag
+    function calcArea() {
+        let base1 = document.getElementById("base").value;
+        let height1 = document.getElementById("height").value;
+        area = 0.5*base1*height1
+        document.getElementById("answer").innerHTML = "Answer:" + area +" cm";
+}
